3d2dedf245ebd524e0fa27a4d1108f5d17158674
RinY0shida <tororo1219@gmail.com>
2024-07-02 11:41:36 +0900

    ✨ add main function for build check

---
+++ robot_embedded_ver1/inc/main.h
@@ -0,0 +1,17 @@
+/**
+ * @file main.h
+ * @author RinYoshida (tororo1219@gmail.com)
+ * @brief robotを動かすためのmain関数を定義するヘッダファイル
+ * @date 2024-07-02
+ */
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#include "can_communication.h"
+
+/**
+ * @brief main関数のloop処理に入っている関数
+ */
+void maintask(void);
+
