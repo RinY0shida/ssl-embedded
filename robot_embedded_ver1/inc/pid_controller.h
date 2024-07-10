52057d05f436cda3f8cac498a5e43e0f41cbc6c1
RinY0shida <tororo1219@gmail.com>
2024-07-10 23:29:07 +0900

    ⚡️ add reset integral

--- robot_embedded_ver1/inc/pid_controller.h
+++ robot_embedded_ver1/inc/pid_controller.h
@@ -15,8 +15,7 @@ public:
     PidController(const double p_gain, const double i_gain, const double d_gain) 
      : p_gain_(p_gain),
        i_gain_(i_gain), 
-       d_gain_(d_gain),
-       {
+       d_gain_(d_gain){
     }
 
     ~PidController(){
@@ -35,6 +34,13 @@ public:
         double differential = (error - pre_error_) / control_cycle;
         integral_ += (error + pre_error_) * control_cycle / 2;
         pre_error_ = error;
+        if (pre_target_value_ != target_value){
+            integral_ = 0;
+        }
+        if (target_value == current_value){
+            integral_ = 0; // TODO 範囲を大きくしてもいい
+        }
+        pre_target_value_ = target_value;
         return p_gain_ * error + i_gain_ * integral_ + d_gain_ * differential;
     }
 
@@ -43,6 +49,7 @@ private:
     const double i_gain_;
     const double d_gain_;
     double pre_error_;
+    double pre_target_value_;
     double integral_;
 };
 #endif //PID_CONTROLLER_H
